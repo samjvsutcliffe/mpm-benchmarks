2d35587f23555e0ec73313e43b0daa76f8c28bd8
Sam <117826225+samjvsutcliffe@users.noreply.github.com>
2022-12-19 14:31:10 +0000

    Updated gimp with glen

--- 2d/gimp/gimp_gen.py
+++ 2d/gimp/gimp_gen.py
@@ -10,6 +10,13 @@ def create_Maxwell3D(self, pset_id=0,density=900,elasticity=1e6,viscosity=1e8):
 	"density": density,
 	"elasticity": elasticity,
 	"viscosity": viscosity})
+def create_Maxwell2D(self, pset_id=0,density=900,elasticity=1e6,viscosity=1e8):
+	self.pset_ids.append(pset_id)
+	self.materials.append({"id": len(self.materials),
+	"type": "Maxwell2D",
+	"density": density,
+	"elasticity": elasticity,
+	"viscosity": viscosity})
 
 def create_NortonHoff2D(self, pset_id=0,density=900,youngs_modulus=1e6,poisson_ratio=0.3,viscosity=1e8,viscous_power=1):
 	self.pset_ids.append(pset_id)
@@ -18,6 +25,16 @@ def create_NortonHoff2D(self, pset_id=0,density=900,youngs_modulus=1e6,poisson_r
 	"density": density,
 	"youngs_modulus": youngs_modulus,
 	"poisson_ratio": poisson_ratio,
+	"viscosity": viscosity,
+	"viscous_power":viscous_power 
+        })
+
+def create_Glen2D(self, pset_id=0,density=900,bulk_modulus=1e6,viscosity=1e8,viscous_power=1):
+	self.pset_ids.append(pset_id)
+	self.materials.append({"id": len(self.materials),
+	"type": "Glen2D",
+	"density": density,
+	"bulk_modulus": bulk_modulus,
 	"viscosity": viscosity,
 	"viscous_power":viscous_power 
         })
@@ -34,7 +51,7 @@ def create_Newtonian3D(self, pset_id=0, density=1.225,
 # The usual start of a PyCBG script:
 sim = utl.Simulation(title="gimp")
 
-resolution = 10
+resolution = 50 
 resolutions = [resolution,resolution ]
 
 # Creating the mesh:
@@ -46,7 +63,7 @@ node_type = "ED2Q16G"
 sim.create_mesh(dimensions=domain_dims, ncells=[x//r for x,r in zip(domain_dims,resolutions)],cell_type = node_type)
 pmesh = utl.Mesh(dimensions=particle_dims,origin=(0,0,0), ncells=[x//r for x,r in zip(particle_dims,resolutions)],cell_type =node_type)
 
-#Pseudo-2d
+#Pseudo-2K
 #sim.create_mesh(dimensions=domain_dims, ncells=(domain_dims[0]//resolution,domain_dims[1]//resolution,1))
 #particle_dims = (500,100,1)
 #domain_dims = (600.,200.,1.)
@@ -54,8 +71,8 @@ pmesh = utl.Mesh(dimensions=particle_dims,origin=(0,0,0), ncells=[x//r for x,r i
 #pmesh = utl.Mesh(dimensions=particle_dims, ncells=(particle_dims[0]//resolution,particle_dims[1]//resolution,1))
 # Creating Material Points, could have been done by filling an array manually:
 #sim.create_particles(npart_perdim_percell=1)
-sim.particles = utl.Particles(pmesh,2,directory=sim.directory)
 mps_per_cell = 2
+sim.particles = utl.Particles(pmesh,mps_per_cell,directory=sim.directory)
 eff_res = [r/mps_per_cell for r in resolutions]
 origin = [r/(2*mps_per_cell) for r in resolutions]
 
@@ -75,7 +92,8 @@ sim.particles.write_file()
 sim.init_entity_sets()
 maxwell_particles = sim.entity_sets.create_set(lambda x,y: True, typ="particle")
 
-E = 1e8
+K = 8e9
+E = 9e9
 nu = 0.325
 density = 900
 
@@ -85,11 +103,20 @@ density = 900
 #sim.materials.create_Newtonian3D(pset_id=maxwell_particles)
 #create_Newtonian3D(sim.materials,pset_id=maxwell_particles)
 #create_Maxwell3D(sim.materials,pset_id=maxwell_particles)
-create_NortonHoff2D(sim.materials,pset_id=maxwell_particles,
+#create_NortonHoff2D(sim.materials,pset_id=maxwell_particles,
+#        density=900,
+#        youngs_modulus=E,
+#        poisson_ratio=0.45,
+#        viscosity=1e-30,viscous_power=3)
+#create_Maxwell2D(sim.materials,pset_id=maxwell_particles,
+#        density=900,
+#        elasticity=E,
+#        viscosity=1e07)
+create_Glen2D(sim.materials,pset_id=maxwell_particles,
         density=900,
-        youngs_modulus=E,
-        poisson_ratio=0.45,
-        viscosity=1e-30,viscous_power=3)
+        bulk_modulus=K,
+        viscosity=110e6,
+        viscous_power=3)
 
 # Boundary conditions on nodes entity sets (blocked displacements):
 walls = []
@@ -108,7 +135,7 @@ sim.set_analysis_parameters(dt=dt,type="MPMExplicit2D", nsteps=nsteps,
         
 
 #sim.analysis_params["damping"] = {"type": "Viscous", "damping_factor": 1e8}
-sim.analysis_params["damping"] = {"type": "Viscous", "damping_factor": E*1e-3}
+sim.analysis_params["damping"] = {"type": "Viscous", "damping_factor": K*1e-2}
 #sim.analysis_params["damping"] = {"type": "Cundall", "damping_factor": 0.05}
 sim.post_processing["vtk"] = ["stresses","volume"]
 
