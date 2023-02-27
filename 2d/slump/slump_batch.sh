70ff9c6a55aaa53b59e2c629cea8d3a9369ccd4d
Sam <117826225+samjvsutcliffe@users.noreply.github.com>
2023-02-27 13:58:54 +0000

    Added slumping

--- 2d/slump/slump_batch.sh
+++ 2d/slump/slump_batch.sh
@@ -3,4 +3,5 @@ python3 slump_gen.py
 echo "Removing files"
 rm slump/results/slump/*
 echo "Running code"
-/mnt/d/Github/mpm/build/mpm -p 8 -i ./slump/input_file.json -f ./ | tee out.txt
+export OMP_SCHEDULE="static"
+/mnt/d/Github/mpm/build/mpm -i ./slump/input_file.json -f ./ | tee out.txt
